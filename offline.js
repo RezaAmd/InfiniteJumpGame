9c58fa29460964990b62eb0c4116f169602b0195
Reza Ahmadi <reza3d75@hotmail.com>
2021-08-22 17:20:52 +0430

    initialize

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1629636345,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/platform-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"style.css"
+	]
+}
